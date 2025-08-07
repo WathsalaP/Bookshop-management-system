e04b095a41248932c75c3f2eb52a9dd174b7ff74
Wathsala Prathapage <wathsalaprathapage@gmail.com>
2025-08-07 17:31:04 +0530

    implemented delete function to row

--- demo/demo/src/main/java/com/pahana_edu/demo/controller/ItemController.java
+++ demo/demo/src/main/java/com/pahana_edu/demo/controller/ItemController.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
-//import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -18,7 +17,6 @@ import com.pahana_edu.demo.dto.ItemDTO;
 import com.pahana_edu.demo.service.ITemService;
 
 @RestController
-// @CrossOrigin
 @CrossOrigin("*")
 @RequestMapping(value = "api/v1")
 
--- frontend/login.js
+++ frontend/login.js
@@ -3,7 +3,7 @@ function validate() {
     const password = document.getElementById("password").value.trim();
 
     if (username === "Sanduni" && password === "123") {
-        window.location.replace("index.html"); // Redirect to the new index.html
+        window.location.replace("customer-details.html"); // Redirect to the new index.html
         return false; // Prevent form from submitting the default way
     } else {
         alert("Login failed. Please check your credentials.");
